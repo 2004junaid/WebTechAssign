1146b21c4dcc0762971cf7aecfe45acfe2296847
2004junaid <mj9394260@gmail.com>
2024-12-19 11:33:55 +0530

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Example: Navigation active state
+document.querySelectorAll('nav ul li a').forEach(link => {
+    link.addEventListener('click', function () {
+        document.querySelector('nav ul li a.active')?.classList.remove('active');
+        this.classList.add('active');
+    });
+});
